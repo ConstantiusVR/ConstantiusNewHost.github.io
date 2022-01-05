1b63b22434eeff68531e5a5c19fad13b460b1a6d
Constantine Ratkin <r-const@yandex.ru>
2022-01-05 15:19:25 +0300

    Initial Commit

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Константин Раткин on 05.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
